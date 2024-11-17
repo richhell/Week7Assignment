90fed68084c3ea56df9fbd65af3aa30dd2c43dca
richhell <richardhellinga@gmail.com>
2024-11-16 21:18:21 -0500

    Completed (mostly)

--- week07.js
+++ week07.js
@@ -67,3 +67,85 @@ repeatWord("Yaddah", 4)
 
 // Question 8
 
+function fullName(firstName, lastName) {
+    console.log(firstName + " " + lastName)
+}
+fullName("Arthur", "Dent")
+
+// Question 9
+
+let array2 = [4, 25, 32, 17]
+let array3 = [1, 17, 37, 51, 99]
+
+function greaterThanHundred(arr) {
+    let arraySum = 0
+    for (let i = 0; i < arr.length; i++) {
+        arraySum += arr[i]; 
+    }
+    if (arraySum > 100) {
+        console.log(arraySum, "true");
+        return true; 
+    } else {
+        console.log(arraySum, "false");
+        return false;
+    }
+
+}
+greaterThanHundred(array2)
+greaterThanHundred(array3)
+
+// Question 10
+
+function numbers(arrnums) {
+    let arraySum = 0;
+    for (let i = 0; i < arrnums.length; i++) {
+    arraySum += arrnums[i];
+    }
+    console.log("The average of the numbers in the array is:", arraySum / arrnums.length);
+    
+}
+ numbers(array3)
+
+ // Question 11
+
+ function compareArray(arr1, arr2) {
+    let arr1Sum = 0;
+    let arr2Sum = 0;
+    for (let i = 0; i < arr1.length; i++) {
+        arr1Sum += arr1[i]; 
+    }
+    for (let i = 0; i < arr2.length; i++) {
+        arr2Sum += arr2[i]; 
+    }
+
+    let arr1Avg = arr1Sum / arr1.length;
+    let arr2Avg = arr2Sum / arr2.length;
+    if (arr1Avg > arr2Avg) {
+        console.log(arr1Avg, ">", arr2Avg, "True")
+        return true; 
+    } else {
+        console.log(arr1Avg, ">", arr2Avg, "False")
+        return false;
+    }
+
+ }
+
+ compareArray(array3, array2)
+
+ // Question 12
+
+ function willBuyDrink(isHotOutside, moneyInPocket) {
+    if (isHotOutside == "yes" && moneyInPocket > 10.5) {
+        console.log("true - I will buy a drink.");
+        return true;
+    } else {
+        console.log("false - I will not buy a drink.");
+        return false;
+    }
+ }
+
+ willBuyDrink("yes", 10)
+
+ // Question 13
+
+ 
